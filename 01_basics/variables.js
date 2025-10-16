f1d3ca401960f235ad9f4cdd969312d953817864
ashishn08 <ashishanshu211@gmail.com>
2025-10-16 00:14:42 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId= 144553 // const cannot be changed 
+let accountEmail="asius@gnail.com"
+var accountPassword="1234"
+accountCity="Jaipur"
+let accountstate; // no value assigned means by default undefined 
+
+accountEmail = "nu@gmail"
+accountPassword="3454"
+accountCity="Kolkata"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity,accountstate])
+// let is used usually not var because of use in scope
+
+
