21dbc2c23b89c5735c4e33f9a350772af9d4da66
ashishn08 <ashishanshu211@gmail.com>
2025-10-17 02:12:32 +0000

    day3

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,16 @@
+let score="33abc"
+console.log(typeof score)
+
+let valueInNumber=Number(score) //string to number convert 
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber) //NaN not a Number 
+// if score = null ,, output=0
+// if score = undefined,, out- NaN
+ 
+let isLoggedin="ashish"
+let islogin=Boolean(isLoggedin)
+console.log(islogin) //true
+// 1 = true , 0 false
+//"" = false
+//"asdf" true
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all JS code as newer version
+// alert(3+3) we are using nodejs not browser
+let name="Ashish"
+let age=10
+let isLoggedin=true
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean
+//null -- standalone value hai along with datatype
+// let state = null 
+//undefined -- variable value not given 
+//symbol -- unique 
+
+// object 
+console.log(typeof null); //object
+console.log(typeof undefined) //undefined 
